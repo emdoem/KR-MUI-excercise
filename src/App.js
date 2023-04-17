9304a5b476fc0a63d46b4d876c79b9383e6d2cb8
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-17 10:57:47 +0200

    install MUI icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Button from '@mui/material/Button';
+import { Icon, IconButton, Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <Button variant="contained">Read more</Button>
+      <Container>
+        <IconButton>
+          <Icon>favorite</Icon>
+        </IconButton>
+        <IconButton>
+          <Icon>share</Icon>
+        </IconButton>
+        <Button variant="outlined" color="primary">Read more</Button>
+      </Container>
     </div>
   );
 }
