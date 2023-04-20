f49de9c4170152c70d90875a25fc6deb1068b165
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-20 11:49:24 +0200

    export Card component, add margins

--- src/App.js
+++ src/App.js
@@ -7,44 +7,64 @@ import { makeStyles } from 'tss-react/mui';
 //import { CardContent } from '@material-ui/core'; 
 // ^^^ where did this come from? Generates hook-error on compiling
 
-// makeStyles needs to be double-called 
+// makeStyles needs to be 'double-called'
 const useStyles = makeStyles()({
   media: {
     height: 200
+  },
+  readMore: {
+    marginLeft: "auto !important"
+  },
+  avatar: {
+    backgroundColor: "orangered"
+  },
+  card: {
+    margin: "10px"
   }
 })
 
-function App() {
+function ArticleCard(props) {
   const {classes} = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        title="John Doe"
+        subheader="Joined 2019-06-08"
+        avatar={<Avatar className={classes.avatar}>JD</Avatar>}
+      />
+      <CardMedia
+        className={classes.media}
+        image="https://picsum.photos/600/400"
+      />
+      <CardContent>
+      <Typography variant="h6">Clickbait title!</Typography>
+      <Typography variant="body2">Some fascinating stinkpiece on a hot topic...</Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+          <Icon>favorite</Icon>
+        </IconButton>
+        <IconButton>
+          <Icon>share</Icon>
+        </IconButton>
+        <Button className={classes.readMore} variant="outlined" color="primary">Read more</Button>
+      </CardActions>
+    </Card>
+  )
+};
+
+function App() {
 
   return (
     <div className="App">
       <CssBaseline />
       <Container>
-        <Card>
-          <CardHeader
-            title="John Doe"
-            subheader="Joined 2019-06-08"
-            avatar={<Avatar>JD</Avatar>}
-          />
-          <CardMedia
-            className={classes.media}
-            image="https://picsum.photos/600/400"
-          />
-          <CardContent>
-          <Typography variant="h6">Clickbait title!</Typography>
-          <Typography variant="body2">Some fascinating stinkpiece on a hot topic...</Typography>
-          </CardContent>
-          <CardActions>
-            <IconButton>
-              <Icon>favorite</Icon>
-            </IconButton>
-            <IconButton>
-              <Icon>share</Icon>
-            </IconButton>
-            <Button variant="outlined" color="primary">Read more</Button>
-          </CardActions>
-        </Card>
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+
       </Container>
     </div>
   );
